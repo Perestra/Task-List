45c2f72a50995d1c894f7a8324a2f18d78816f68
Perestra <danilo.ps.1996@gmai.com>
2022-10-26 22:41:51 -0300

    Added new elements to creation

--- script/script.js
+++ script/script.js
@@ -8,6 +8,9 @@ function crudActions(value) {
     const liTag = document.createElement('li')
     liTag.classList.add('item')
 
+    const itemName = document.createElement('div')
+    itemName.classList.add('item-name')
+
     const inputCheckBox = document.createElement('input')
     inputCheckBox.setAttribute('type', 'checkbox')
     inputCheckBox.classList.add('input-checkbox')
@@ -15,26 +18,32 @@ function crudActions(value) {
     const h2Tag = document.createElement('h2')
     h2Tag.innerText = value
 
+
+    const itemButton = document.createElement('div')
+    itemButton.classList.add('item-button')
+
     const editButton = document.createElement('button')
     editButton.classList.add('edit-button')
-    editButton.innerHTML = '<span class="material-symbols-outlined">edit</span>'
+    editButton.innerHTML = '<i class="fa-solid fa-pen"></i>'
 
     const deleteButton = document.createElement('button')
     deleteButton.classList.add('delete-button')
-    deleteButton.innerHTML = '<span class="material-symbols-outlined">delete</span>'
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
 
-    liTag.appendChild(inputCheckBox)
-    liTag.appendChild(h2Tag)
-    liTag.appendChild(editButton)
-    liTag.appendChild(deleteButton)
+    liTag.appendChild(itemName)
+    itemName.appendChild(inputCheckBox)
+    itemName.appendChild(h2Tag)
+    liTag.appendChild(itemButton)
+    itemButton.appendChild(editButton)
+    itemButton.appendChild(deleteButton)
     ulTag.appendChild(liTag)
 
     editButton.addEventListener('click', () => {
         h2Tag.toggleAttribute('contenteditable')
         if(h2Tag.hasAttribute('contenteditable')) {
-            editButton.innerHTML = '<span class="material-symbols-outlined">done</span>'
+            editButton.innerHTML = '<i class="fa-solid fa-check"></i>'
         }else {
-            editButton.innerHTML = '<span class="material-symbols-outlined">edit</span>'
+            editButton.innerHTML = '<i class="fa-solid fa-pen"></i>'
         }
     })
 
